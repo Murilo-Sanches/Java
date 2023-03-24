bf85ead51a63cb85869132a91aa6571c7b54ec02
Murilo-Sanches <sanchesmurilo199@outlook.com>
2023-03-24 16:41:32 -0300

    maven structure

---
+++ src/main/java/com/ms/App.java
@@ -0,0 +1,14 @@
+package com.ms;
+
+/**
+ * Classe App, a primeira classe depois do package (ms) e a única com método
+ * main(string[] args)
+ * Ponto de partida de todo programa Java, a função main é a primeira coisa que
+ * é
+ * executada!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
